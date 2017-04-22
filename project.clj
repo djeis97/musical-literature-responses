54c19e5294b904ac33e2b6a504f46ea08b72c2c8
Elijah Malaby <qwe12345678910@gmail.com>
2017-04-22 15:22:48 -0400

    Leipzig project

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject musical-literature-responses "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.1"]
+                 [leipzig "0.10.0"]])
