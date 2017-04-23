21225548c10dd3abaff6a83d0185a5f22529af36
Elijah Malaby <qwe12345678910@gmail.com>
2017-04-22 20:59:36 -0400

    Deps updates

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject musical-literature-responses "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
-                 [leipzig "0.10.0"]])
+                 [leipzig "0.10.0"]]
+  :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]])
